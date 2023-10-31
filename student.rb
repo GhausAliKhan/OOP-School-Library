181c0a8ec03d46a8c1e663739cfceae84b074cc7
GhausAliKhan <ghauskhan1616@gmail.com>
2023-10-31 15:17:35 +0500

    extend parent's constructor by adding @classroom and a parameter for it

--- student.rb
+++ student.rb
@@ -2,4 +2,8 @@ require_relative 'person'
 
 # Class Student inherited from Person
 class Student < Person
+  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
+    @classroom = classroom
+  end
 end
