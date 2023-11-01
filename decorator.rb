431947dec9e25c7e0a9b4dc38512626963a10a04
GhausAliKhan <ghauskhan1616@gmail.com>
2023-11-01 14:48:18 +0500

    add method correct_name

--- decorator.rb
+++ decorator.rb
@@ -2,4 +2,12 @@ require_relative 'nameable'
 
 # This class serves as the base decorator for other specific decorators.
 class BaseDecorator < Nameable
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
 end
