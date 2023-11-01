611dcacc703ee48442ba0bd1cd2ecca7c20c8e02
GhausAliKhan <ghauskhan1616@gmail.com>
2023-11-01 14:52:31 +0500

    Implement a method correct_name

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -2,4 +2,7 @@ require_relative 'decorator'
 
 # This class serves to trim the name of a Person object to a maximum of 10 characters.
 class TrimmerDecorator < BaseDecorator
+  def correct_name
+    name = @nameable.correct_name
+  end
 end
