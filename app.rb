b265b80ab4071f6284c304fa3cec4dbcbba3dc50
GhausAliKhan <ghauskhan1616@gmail.com>
2023-11-03 15:10:19 +0500

    Add logic for method create_book

--- app.rb
+++ app.rb
@@ -72,6 +72,14 @@ class App
   end
 
   def create_book
+    print 'Title: '
+    title = gets.chomp
+    print 'Author: '
+    author = gets.chomp
+
+    new_book = Book.new(title, author)
+    @books << new_book
+    puts 'Book created successfully'
   end
 
   def create_rental
