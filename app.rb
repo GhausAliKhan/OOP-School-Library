b3fa9311727794ef843f44dec333cee801c784c8
GhausAliKhan <ghauskhan1616@gmail.com>
2023-11-03 15:14:39 +0500

    Add logic for method list_books and list_people

--- app.rb
+++ app.rb
@@ -120,9 +120,17 @@ class App
   end
 
   def list_books
+    puts 'Select a book from the following list by number'
+    @books.each_with_index do |book, index|
+      puts "#{index}) Title: \"#{book.title}\", Author: #{book.author}"
+    end
   end
 
   def list_people
+    puts "\nSelect a person from the following list by number (not id)"
+    @people.each_with_index do |person, index|
+      puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
   end
 
   def create_new_rental
