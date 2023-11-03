a35ce04fb7507492bb0bffb2d5f35d75bdd3f6e0
GhausAliKhan <ghauskhan1616@gmail.com>
2023-11-03 14:50:40 +0500

    Create app.rb and add methods for our app

---
+++ app.rb
@@ -0,0 +1,54 @@
+require_relative 'book'
+require_relative 'rental'
+require_relative 'student'
+require_relative 'teacher'
+
+# Class containing app functionality for a basic UI
+class App
+  def initialize
+    @books = []
+    @people = []
+  end
+
+  def list_all_books
+  end
+
+  def list_all_people
+  end
+
+  def create_person
+  end
+
+  def create_student
+  end
+
+  def create_teacher
+  end
+
+  def create_book
+  end
+
+  def create_rental
+  end
+
+  def valid_conditions_for_rental?
+  end
+
+  def select_book
+  end
+
+  def select_person
+  end
+
+  def list_books
+  end
+
+  def list_people
+  end
+
+  def create_new_rental
+  end
+
+  def list_all_rentals_for_person
+  end
+end
