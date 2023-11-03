6a5a2218d766c8fc6b40f84cbe65700b561d1b18
GhausAliKhan <ghauskhan1616@gmail.com>
2023-11-03 14:47:22 +0500

    Add method execute_choice which executes user's selected action

--- main.rb
+++ main.rb
@@ -10,3 +10,25 @@ def display_menu
   puts '6 - List all rentals for a given person id'
   puts '7 - Exit'
 end
+
+def execute_choice(app, choice)
+  actions = {
+    '1' => -> { app.list_all_books },
+    '2' => -> { app.list_all_people },
+    '3' => -> { app.create_person },
+    '4' => -> { app.create_book },
+    '5' => -> { app.create_rental },
+    '6' => -> { app.list_all_rentals_for_person },
+    '7' => lambda {
+      puts 'Thank you for using this app!'
+      exit
+    }
+  }
+
+  action = actions[choice]
+  if action
+    action.call
+  else
+    puts 'Invalid option. Please try again.'
+  end
+end
