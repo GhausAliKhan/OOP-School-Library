2c4e07990830960f4eeb2838ddc1d6a62e924f7c
GhausAliKhan <ghauskhan1616@gmail.com>
2023-11-03 15:06:36 +0500

    add logic for method create_person, create_student and create_teacher

--- app.rb
+++ app.rb
@@ -33,12 +33,42 @@ class App
   end
 
   def create_person
+    puts 'Do you want to create a student (1) or a teacher (2)? [Input the number]:'
+    choice = gets.chomp
+    case choice
+    when '1'
+      create_student
+    when '2'
+      create_teacher
+    else
+      puts 'Invalid option. No person was created.'
+    end
   end
 
   def create_student
+    print 'Age: '
+    age = gets.chomp.to_i
+    print 'Name: '
+    name = gets.chomp
+    print 'Has parent permission? [Y/N]: '
+    parent_permission = gets.chomp.downcase == 'y'
+
+    new_student = Student.new(age, name, parent_permission: parent_permission)
+    @people << new_student
+    puts 'Student created successfully'
   end
 
   def create_teacher
+    print 'Age: '
+    age = gets.chomp.to_i
+    print 'Name: '
+    name = gets.chomp
+    print 'Specialization: '
+    specialization = gets.chomp
+
+    new_teacher = Teacher.new(specialization, age, name)
+    @people << new_teacher
+    puts 'Teacher created successfully'
   end
 
   def create_book
