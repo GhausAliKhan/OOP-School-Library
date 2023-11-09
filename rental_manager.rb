72011fb81cb177bb6f4ca7a0081a2e1893a778cc
GhausAliKhan <ghauskhan1616@gmail.com>
2023-11-09 16:33:07 +0500

    Update class RentalManager to load and save rental data

--- rental_manager.rb
+++ rental_manager.rb
@@ -1,10 +1,13 @@
+require 'json'
 # Class responsible for rental management
 class RentalManager
-  attr_accessor :book_manager, :person_manager
+  attr_accessor :book_manager, :person_manager, :rentals
 
   def initialize(book_manager, person_manager)
     @book_manager = book_manager
     @person_manager = person_manager
+    @rentals = []
+    load_rentals_from_json if File.exist?('rentals.json')
   end
 
   # Select book from available books
@@ -25,9 +28,44 @@ class RentalManager
     person_index
   end
 
+  def save_rentals_to_json
+    File.open('rentals.json', 'w') do |f|
+      data_to_write = @rentals.map(&:to_json).to_json
+      f.write(data_to_write)
+    end
+  end
+
+  def load_rentals_from_json
+    json_data = File.read('rentals.json')
+    array_of_strings = JSON.parse(json_data)
+    array_of_strings.each { |string| process_rental(string) }
+  rescue JSON::ParserError => e
+    puts "Could not parse rentals.json: #{e.message}"
+  end
+
+  def process_rental(string)
+    hash = JSON.parse(string)
+    book = find_book(hash)
+    person = find_person(hash)
+    create_rental(hash, book, person) if book && person
+  end
+
+  def find_book(hash)
+    @book_manager.books.find { |b| b.title == hash['book_title'] && b.author == hash['book_author'] }
+  end
+
+  def find_person(hash)
+    @person_manager.people.find { |p| p.id == hash['person_id'] }
+  end
+
+  def create_rental(hash, book, person)
+    @rentals << Rental.new(hash['date'], book, person)
+  end
+
   def create_new_rental(book_index, person_index, date)
     new_rental = Rental.new(date, @book_manager.books[book_index], @person_manager.people[person_index])
     @book_manager.books[book_index].add_rental(new_rental)
+    @rentals << new_rental
     puts 'Rental created successfully'
   end
 
